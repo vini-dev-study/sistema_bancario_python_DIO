d8a68af4b76e4a64c8ab08e5c34d93863210e0d3
Vinicius Gomes <pain-nici2011@live.com>
2025-02-28 11:53:44 -0300

    refactoring

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -7,38 +7,55 @@ menu = """
 
 => """
 
+numero_limite_saque = 3
+numero_saque = 0
+valor_limite_saque = 500
 saldo = 0
-limite = 500
 extrato = []
-numero_saques = 0
-limite_saques = 3
 
+def depositar():
+    global extrato
+    global saldo
+
+    valor = float(input('Qual valor deseja depositar?'))
+    saldo += valor
+    extrato.append(f"Deposito no valor de R$ {valor:.2f}")
+
+
+def sacar():
+    global valor_limite_saque
+    global saldo
+    global extrato
+
+    valor = float(input('Qual valor deseja sacar?'))
+
+    if valor > valor_limite_saque:
+        print(f'O valor R$ {valor:.2f} está acima do limite diário de R$ {valor_limite_saque:.2f}')
+    elif saldo >= valor:
+        saldo -= valor
+        extrato.append(f"Saque no valor de R$ {valor:.2f}")
+
+    else:
+        print("Saldo insuficiente!")
+    
 while True:
     opcao = input(menu)
+
     if opcao == 'd':
-        valor = float(input('Digite o valor a ser depositado: \n'))
-        saldo += valor
-        extrato.append(f'Depósito: R$ {valor:.2f}')
+        depositar()
     elif opcao == 's':
-        if numero_saques < limite_saques:
-            valor = float(input('Digite o valor a ser sacado: \n'))
-            
-            if valor > limite:
-                print('\nValor de saque acima do limite diário.')
-            elif saldo >= valor:
-                saldo -= valor
-                extrato.append(f'Saque: R$ {valor:.2f}')
-                numero_saques += 1
-            else:
-                print('Saldo insuficiente.')
+        if numero_saque < numero_limite_saque:
+            sacar()
+            numero_saque += 1
         else:
-            print('Limite de saques atingido.')
+            print('Limite de saque atingido!')
     elif opcao == 'e':
-        print(f'Saldo: R$ {saldo:.2f} \n')
-        print('Extrato:')
+        print(f'O saldo é de: R$ {saldo:.2f} \n')
+        print('Extrato: \n')
+
         for operacao in extrato:
             print(operacao)
     elif opcao == 'q':
         break
     else:
-        print('Opção inválida.')
+        print('Opção inválida. Tente novamente!')
